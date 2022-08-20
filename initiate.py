ccca40e1d12d2b28b6d0567801c6577300c3953c
Ross McLean <ri.mcl@icloud.com>
2022-08-20 12:37:19 +0100

    Update initiate.py - Implement create_dynamodb_table function - Implement create_sqs_queue function

---
+++ initiate.py
@@ -0,0 +1,43 @@
+import boto3
+
+# Create DynamoDB table
+def create_dynamodb_table(table_name):
+    dynamodb = boto3.resource('dynamodb')
+    table = dynamodb.create_table(
+        TableName=table_name,
+        KeySchema=[
+            {
+                'AttributeName': 'Image_Name',
+                'KeyType': 'HASH'
+            }
+        ],
+        AttributeDefinitions=[
+            {
+                'AttributeName': 'Image_Name',
+                'AttributeType': 'S'
+            }
+        ],
+        ProvisionedThroughput={
+            'ReadCapacityUnits': 5,
+            'WriteCapacityUnits': 5
+        }
+    )
+    print("Table status:", table.table_status)
+
+    if table.table_status == 'ACTIVE':
+        print("Table created successfully.")
+    else:
+        print("Table creation failed.")
+
+# Create SQS Standard Queue
+def create_sqs_queue(queue_name):
+    sqs = boto3.resource('sqs')
+    queue = sqs.create_queue(QueueName=queue_name)
+
+    if queue.attributes.get('QueueArn'):
+        print("Queue created successfully.")
+    else:
+        print("Queue creation failed.")
+
+create_dynamodb_table('diet2_s2030507_table')
+create_sqs_queue('diet2_s2030507_queue')
