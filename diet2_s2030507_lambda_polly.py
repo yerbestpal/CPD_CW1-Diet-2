90ef1d970b0114be11b22cae37412d3e66797d67
Ross McLean <ri.mcl@icloud.com>
2022-08-21 18:40:11 +0100

    Update lambda script - Add sentiment detection using Comprehend - Save sentiment to DynamoDB table - Send SNS email notifications

--- diet2_s2030507_lambda_polly.py
+++ diet2_s2030507_lambda_polly.py
@@ -2,14 +2,48 @@ import json
 import boto3
 
 def lambda_handler(event, context):
-  s3_client = boto3.client('s3')
   s3 = boto3.resource('s3')
   bucket = s3.Bucket('diet2-s2030507-bucket')
 
   for obj in bucket.objects.all():
     key = obj.key
-    body = obj.get()['Body'].read()
+    file = s3.get_object(Bucket='diet2_s2030507_bucket', Key=key)
+    body = str(file['Body'].read())
     if event['Records'][0]['body'] == key:
+
+      # Detect sentiment of text using Comprehend.
+      comprehend = boto3.client('comprehend')
+      sentiment = comprehend.detect_sentiment(
+        Text=body,
+        LanguageCode='en'
+      )
+      print(sentiment)
+
+      # Save results in DynamoDB.
+      if sentiment.get('Sentiment') == 'POSITIVE' or \
+      sentiment.get('Sentiment') == 'NEGATIVE' or \
+      sentiment.get('Sentiment') == 'MIXED':
+        dynamodb = boto3.resource('dynamodb')
+        table = dynamodb.Table('diet2_s2030507_table')
+        table.put_item(
+          Item = {
+            'Image_Name': key,
+            'Sentiment': sentiment
+          }
+        )
+        print('Saved sentiment in DynamoDB.')
+
+        # Send notification of NEGATIVE sentiment to SNS
+        if sentiment.get('Sentiment') == 'NEGATIVE':
+          sns = boto3.client('sns')
+          sns.publish(
+            TopicArn='arn:aws:sns:us-east-1:973567983713:diet2-s2030507-sns',
+            Message=json.dumps({'default': json.dumps(sentiment)}),
+            Subject='File upload status'
+          )
+          print('Sent notification to SNS.')
+
+      # Convert text to speech using Polly.
       client = boto3.client('polly')
       output = client.synthesize_speech(
         OutputFormat='mp3',
